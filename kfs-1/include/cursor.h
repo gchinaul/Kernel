0890a8e43cb5aa80a7c6dc3760df52a2d12c09ad
pepper <70756191+jack-pepper@users.noreply.github.com>
2026-06-06 16:08:26 +0200

    reinit

--- kfs-1/include/cursor.h
+++
@@ -1,7 +0,0 @@
-#ifndef CURSOR_H
-# define CURSOR_H
-
-void enable_cursor(uint8_t cursor_start, uint8_t cursor_end);
-void update_cursor(int x, int y);
-
-#endif
--- kfs-1/include/libft.h
+++
@@ -1,7 +0,0 @@
-#ifndef LIBFT_H
-# define LIBFT_H
-
-void		*kmemset(void *dst, int c, size_t n);
-size_t	strlen(const char* str);
-
-#endif
